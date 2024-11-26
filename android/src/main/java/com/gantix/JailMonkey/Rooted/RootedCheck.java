4f65661c77b355104f834c73f2b7ff4dcd5a690b
Kanad Godse <kanad.godse@holcim.com>
2024-11-26 11:48:43 +0530

    Updated rootbeer and added few new checks

--- android/src/main/java/com/gantix/JailMonkey/Rooted/RootedCheck.java
+++ android/src/main/java/com/gantix/JailMonkey/Rooted/RootedCheck.java
@@ -39,49 +39,55 @@ public class RootedCheck {
     }
 
     private static class RootBeerResults {
-        private final boolean detectRootManagementApps;
-        private final boolean detectPotentiallyDangerousApps;
+        private final boolean checkRootManagementApps;
+        private final boolean checkPotentiallyDangerousApps;
         private final boolean checkForSuBinary;
         private final boolean checkForDangerousProps;
         private final boolean checkForRWPaths;
-        private final boolean detectTestKeys;
+        private final boolean checkTestKeys;
         private final boolean checkSuExists;
         private final boolean checkForRootNative;
         private final boolean checkForMagiskBinary;
+        private final boolean checkRootCloakingApps;
+        private final boolean checkForRWSystem;
 
         RootBeerResults(Context context) {
             final RootBeer rootBeer = new RootBeer(context);
             rootBeer.setLogging(false);
 
-            detectRootManagementApps = rootBeer.detectRootManagementApps();
-            detectPotentiallyDangerousApps = rootBeer.detectPotentiallyDangerousApps();
+            checkRootManagementApps = rootBeer.checkRootManagementApps();
+            checkPotentiallyDangerousApps = rootBeer.checkPotentiallyDangerousApps();
             checkForSuBinary = rootBeer.checkForSuBinary();
             checkForDangerousProps = rootBeer.checkForDangerousProps();
             checkForRWPaths = rootBeer.checkForRWPaths();
-            detectTestKeys = rootBeer.detectTestKeys();
+            checkTestKeys = rootBeer.checkTestKeys();
             checkSuExists = rootBeer.checkSuExists();
             checkForRootNative = rootBeer.checkForRootNative();
             checkForMagiskBinary = rootBeer.checkForMagiskBinary();
+            checkRootCloakingApps = rootBeer.checkRootCloakingApps();
+            checkForRWSystem = rootBeer.checkForRWSystem();
         }
 
         public boolean isJailBroken() {
-            return detectRootManagementApps || detectPotentiallyDangerousApps || checkForSuBinary
+            return checkRootManagementApps || checkPotentiallyDangerousApps || checkForSuBinary
                     || checkForDangerousProps || checkForRWPaths
-                    || detectTestKeys || checkSuExists || checkForRootNative || checkForMagiskBinary;
+                    || checkTestKeys || checkSuExists || checkForRootNative || checkForMagiskBinary || checkRootCloakingApps || checkForRWSystem;
         }
 
         public Map<String, Object> toNativeMap() {
             final Map<String, Object> map = new HashMap<>();
 
-            map.put("detectRootManagementApps", detectRootManagementApps);
-            map.put("detectPotentiallyDangerousApps", detectPotentiallyDangerousApps);
+            map.put("checkRootManagementApps", checkRootManagementApps);
+            map.put("checkPotentiallyDangerousApps", checkPotentiallyDangerousApps);
             map.put("checkForSuBinary", checkForSuBinary);
             map.put("checkForDangerousProps", checkForDangerousProps);
             map.put("checkForRWPaths", checkForRWPaths);
-            map.put("detectTestKeys", detectTestKeys);
+            map.put("checkTestKeys", checkTestKeys);
             map.put("checkSuExists", checkSuExists);
             map.put("checkForRootNative", checkForRootNative);
             map.put("checkForMagiskBinary", checkForMagiskBinary);
+            map.put("checkRootCloakingApps", checkRootCloakingApps);
+            map.put("checkForRWSystem", checkForRWSystem);
 
             return map;
         }
